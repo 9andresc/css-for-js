8a93155863a8e1be6ae349b23dc5eff5658503d1
Gustavo Andres <andrescabral.c@gmail.com>
2022-04-06 12:00:34 -0400

    chore: Run prettier on every pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,4 +3,6 @@ module.exports = {
   "**/*.ts?(x)": () => "yarn type-check",
   // Run ESLint on changes to JavaScript/TypeScript files
   "**/*.(ts|js)?(x)": (filenames) => `yarn lint . ${filenames.join(" ")}`,
+  // Run Prettier to auto-format the  JavaScript/TypeScript files
+  "**/*.(ts|js)?(x)": (filenames) => `prettier --write . ${filenames.join(" ")}`,
 };
